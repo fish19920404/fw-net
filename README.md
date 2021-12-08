17375e0f77495d7ad1cfdb5847748bc4ea8ba455
yujin <136687211@qq.com>
2021-12-08 23:30:42 +0800

    feat:修改配置文件

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # fw-net
 framework for basic net
 
+12-05
+1、协议层定义
+
 
 1、注意引用方式
 implementation、api
@@ -9,3 +12,5 @@ implementation、api
 PROJ_GROUP
 PROJ_ARTIFACTID
 maven地址 -- 如果不用访问到，可能会引起编译不通过
+
+
--- net-protocol/src/main/java/com/neil/arch/net/protocol/api/FWAPIService.kt
+++ net-protocol/src/main/java/com/neil/arch/net/protocol/api/FWAPIService.kt
@@ -1,10 +1,17 @@
 package com.neil.arch.net.protocol.api
 
+import io.reactivex.Observable
+import retrofit2.http.GET
+
 /**
  * description ： TODO:类的作用
  * author : yujin
  * date : 2021/12/5 14:25
  */
 interface FWAPIService {
+
+    @GET
+    fun <T> get(): Observable<T>
+
 }
 
